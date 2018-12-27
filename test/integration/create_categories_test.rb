1783156e3b69ab10c6eea28462dbb4f5cc6b5689
Jose Pino <joserafa0307@gmail.com>
2018-12-27 14:42:08 +0000

    Add categories model and tests

---
+++ test/integration/create_categories_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class CreateCategoriesTest < ActionDispatch::IntegrationTest
+  
+  test 'get new category form and create category' do
+    get new_category_path
+    assert_template 'categories/new'
+    assert_difference 'Category.count', 1 do
+      post categories_path, params:{ category: {name: "sports"}} 
+      follow_redirect!
+    end
+    assert_template 'categories/index'
+    assert_match "sports", response.body
+    
+  end
+  
+test "invalid category submission results in failure" do
+
+  get new_category_path
+  assert_template 'categories/new'
+  assert_no_difference 'Category.count' do
+    post categories_path, params: { category: {name: " "} }
+  end
+  
+  assert_template 'categories/new'
+  assert_select "h2.panel-title"  
+  assert_select 'div.panel-body'  
+  #assert_match 'prohibited', response.body #la palabra prohibited esta puesta en el panel de mensaje de error, si el test pasa es porque el panel aparece
+  
+  end
+
+end
