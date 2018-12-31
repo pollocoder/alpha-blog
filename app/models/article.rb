6e0e8ab41dd4b782c7f46ff203e76cdc957ff092
Jose Pino <joserafa0307@gmail.com>
2018-12-31 14:25:53 +0000

    Add article and category association

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,10 +2,12 @@ class Article < ApplicationRecord
  
   belongs_to :user
   
+  #las dos siguientes lineas es la asociacion de many_to_many de articulos y categorias
+  has_many :article_categories
+  has_many :categories, through: :article_categories
+  
   validates :title, presence: true, length: { minimum: 3, maximum: 50}
-  
   validates :description, presence: true, length: { minimum: 10, maximum: 200}
-  
   validates :user_id, presence: true
   
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,9 @@
 class Category < ApplicationRecord
+  
+  #las dos siguientes lineas es la asociacion de many_to_many de articulos y categorias
+  has_many :article_categories
+  has_many :articles, through: :article_categories
+  
   validates :name, presence: true, length: { minimum: 3, maximum: 25 }
   validates_uniqueness_of :name
 end
